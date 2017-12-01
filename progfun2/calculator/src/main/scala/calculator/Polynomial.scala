5e4f8c72f9a85f4f2faebb48d04ef2fa47183f81
Igor Yurinok <iyurinok@mz.com>
2017-11-30 22:39:00 -0800

    parprog1

--- progfun2/calculator/src/main/scala/calculator/Polynomial.scala
+++ progfun2/calculator/src/main/scala/calculator/Polynomial.scala
@@ -1,13 +1,22 @@
 package calculator
 
 object Polynomial {
-  def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-    ???
+  def computeDelta(a: Signal[Double],
+                   b: Signal[Double],
+                   c: Signal[Double]): Signal[Double] = {
+    Signal(scala.math.pow(b(), 2) - 4 * a() * c())
   }
 
-  def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+  def computeSolutions(a: Signal[Double],
+                       b: Signal[Double],
+                       c: Signal[Double],
+                       delta: Signal[Double]): Signal[Set[Double]] = {
+    Signal(delta() match {
+      case d if d >= 0 =>
+        Set((-b() + scala.math.sqrt(delta())) / (2 * a()),
+            (-b() - scala.math.sqrt(delta())) / (2 * a()))
+      case d if d == 0 => Set(-b() / (2 * a()))
+      case _           => Set()
+    })
   }
 }
